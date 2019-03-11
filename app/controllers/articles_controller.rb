7a30a26be4877573da734e1db92afc083a922353
letavi <ltviet.nk@gmail.com>
2019-03-11 09:54:59 +0700

    Complete update, destroy article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,12 +21,19 @@ class ArticlesController < ApplicationController
   end
 
   def update
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
 
   def show
   end
 
   def destroy
+    @article.destroy
+    redirect_to articles_path
   end
 
   private
